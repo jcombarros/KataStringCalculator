f7f037907d5d353cb31a67fdecfb3afb409bb995
Javier Combarros <Javier@138.4.192.251>
2015-11-07 20:09:12 +0100

    Add basis tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class CalculatorTest {
+	
+	private Calculator calculator;
+	
+	@Before
+	public void before(){
+		calculator = new Calculator();
+	}
+	
+	@Test
+	public void emptyStringInputTest(){
+		assertEquals(0, calculator.add(""));
+	}
+	
+	@Test
+	public void oneNumberStringInputTest(){
+		assertEquals(1, calculator.add("1"));
+	}
+	
+	@Test
+	public void twoNumbersStringInputTest(){
+		assertEquals(2, calculator.add("1,1"));
+	}
 
 }
