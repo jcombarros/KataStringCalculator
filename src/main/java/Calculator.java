2ecf5167a0349386099ace54e0c24dbf9ed87052
Javier Combarros <Javier@138.4.192.251>
2015-11-07 20:16:45 +0100

    Add method implemented

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Calculator {
 
+	private static final Object EMPTY_STRING = "";
+
 	public int add(String numbers){
-		//TODO
-		return 0;
+		if(numbers == null || numbers.equals(EMPTY_STRING)){
+			return 0;
+		}
+		List<String> values = Arrays.asList(numbers.split(","));
+		int result = 0;
+		for (String value : values) {
+			int intValue = Integer.parseInt(value);
+			result = result += intValue;
+		}
+		return result;
 	} 
+
 }
